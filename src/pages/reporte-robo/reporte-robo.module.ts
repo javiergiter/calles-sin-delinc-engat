d25a9a1a0f7019eabe48cfe6da756449b11e30a0
javier Rivera <javvito57251@gmail.com>
2018-09-06 21:34:43 -0500

    pagina informacion

---
+++ src/pages/reporte-robo/reporte-robo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReporteRoboPage } from './reporte-robo';
+
+@NgModule({
+  declarations: [
+    ReporteRoboPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ReporteRoboPage),
+  ],
+})
+export class ReporteRoboPageModule {}
---
+++ src/pages/robo-reportes/robo-reportes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RoboReportesPage } from './robo-reportes';
+
+@NgModule({
+  declarations: [
+    RoboReportesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RoboReportesPage),
+  ],
+})
+export class RoboReportesPageModule {}
